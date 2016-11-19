c1ffc5bb5b6096075b1ca1e43e1de77dd8ed29c6
Hexagon <robinnilsson@gmail.com>
2016-11-19 10:56:22 +0100

    Work in progress

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # qbus-ipc
 
+[![Build status](https://travis-ci.org/Hexagon/qbus-ipc.svg)](https://travis-ci.org/Hexagon/qbus-ipc)
+
 Work in progress, early stage, untested and possible not working
 
 # License
--- test/test.js
+++ test/test.js
@@ -27,23 +27,67 @@ THE SOFTWARE.
 "use strict";
 
 var should = require("should"),
-	ipc = require("../index.js");
+	ipc = require("../index.js"),
+	address = {
+		tcp: { host: "127.0.0.1", port: 31338 },
+		socket: { path: "/tmp/crdtest.sock" }
+	}
 
-describe("Listening", function () {
+describe("Listening ...", function () {
 
 	it("on tcp should not throw", function (done) {
 		(function(){
-			var server = new ipc(),
-				address = { host: "0.0.0.0", port: 31338 };
-			server.listen(address, (e) => { if(e) throw e; done(); } );
+			var server = new ipc();
+			server.listen(address.tcp, (e) => { if(e) throw e; server.close(done); } );
 		}).should.not.throw();
 	});
 
 	it("on sockets should not throw", function (done) {
 		(function(){
-			var server = new ipc(),
-				address = { path: "/tmp/crdtest.sock" };
-			server.listen(address, (e) => { if(e) throw e; done(e); } );
+			var server = new ipc();
+			server.listen(address.socket, (e) => { if(e) throw e; server.close(done); } );
+		}).should.not.throw();
+	});
+
+});
+
+describe("Connecting ...", function () {
+
+	it("to tcp should not throw", function (done) {
+		(function(){
+
+			// Create server
+			var server = new ipc();
+			server.listen(address.tcp, (e1) => { 
+				if(e1) throw e1; 
+				// Create client
+				var client = new ipc();
+				client.connect(address.tcp, (e2) => { 
+					if(e2) throw e2; 
+					client.close();
+					server.close(done); 
+				});
+			});
+
+		}).should.not.throw();
+	});
+
+	it("to socket should not throw", function (done) {
+		(function(){
+
+			// Create server
+			var server = new ipc();
+			server.listen(address.socket, (e1) => { 
+				if(e1) throw e1; 
+				// Create client
+				var client = new ipc();
+				client.connect(address.socket, (e2) => { 
+					if(e2) throw e2; 
+					client.close();
+					server.close(done); 
+				});
+			});
+
 		}).should.not.throw();
 	});
 
